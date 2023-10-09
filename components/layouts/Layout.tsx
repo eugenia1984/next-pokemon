6200c2fb32d9c9c0a00cab776f93191e192ae7ff
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-09 11:58:28 -0300

    -feat: creo el layout

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
+import Head from 'next/head'
 
-export const Layout = () => {
+interface LayoutProps {
+  children: ReactNode
+  title?: string
+}
+
+export const Layout: FC<LayoutProps> = ({ children, title }) => {
   return (
     <>
-    
+      <Head>
+        <title>{ title || 'Pokemon App' }</title>
+        <meta name="author" content="María Eugenia Costa" />
+        <meta name="description" content={ `Información sobre el Pokemon: ${ title }` } />
+        <meta name="keywords" content={ `${ title }, pokemon, pokedex` } />
+      </Head>
+      { /* Navbar*/ }
+      <main>
+        { children }
+      </main>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react'
+import { Layout } from '../components/layouts'
 import { Button } from '@nextui-org/react'
 
 const HomePage:FC = () => {
   return (
-    <>
+    <Layout title='Listado de Pokemons'>
       <Button color="gradient">Click me</Button>
-    </>
+    </Layout>
   )
 }
 
